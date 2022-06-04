244c759c09aab6dcad1960474399f43c116e3844
Oğuz Can Yılmazlar <ogzylmzlr26@yahoo.com>
2022-06-05 00:09:47 +0300

    Update GUIManager.java

--- src/main/java/me/acablade/bladegui/GUIManager.java
+++ src/main/java/me/acablade/bladegui/GUIManager.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Getter
-public class GUIManager{
+public class GUIManager {
 
 	private List<Runnable> runnableList = new ArrayList<>();
 	private final JavaPlugin plugin;
