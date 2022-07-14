2d2452fd72ec8aa9d6977ebb1ed9deb3ef61060d
Oğuz Can Yılmazlar <ogzylmzlr26@yahoo.com>
2022-07-14 04:49:13 +0300

    update gui

--- src/main/java/me/acablade/bladegui/GUI.java
+++ src/main/java/me/acablade/bladegui/GUI.java
@@ -111,7 +111,11 @@ public class GUI implements Runnable, Listener, InventoryHolder {
 
 	@EventHandler
 	public void onClick(InventoryClickEvent event){
-		if(event.getClickedInventory()!=event.getView().getTopInventory()||event.isShiftClick()) return;
+		if(event.isShiftClick()) {
+			event.setResult(Event.Result.DENY);
+			return;
+		}
+		if(event.getClickedInventory()!=event.getView().getTopInventory()) return;
 		if(!this.viewers.contains(event.getWhoClicked().getUniqueId())) return;
 		int slot = event.getSlot();
 		int[] coords = toCoordinate(slot);
